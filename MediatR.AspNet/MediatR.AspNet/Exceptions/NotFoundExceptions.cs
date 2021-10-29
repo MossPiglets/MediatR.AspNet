73d717de490fb346ae72bc2d778ec46bfcf53772
LAPTOP-1BL52TMD\kasia <kasiakadziolka@gmail.com>
2021-10-29 18:24:24 +0200

    DeleteNotAllowedException

--- MediatR.AspNet/MediatR.AspNet/Exceptions/NotFoundExceptions.cs
+++
@@ -1,7 +0,0 @@
-using System;
-
-namespace MediatR.AspNet.Exceptions {
-	public class NotFoundExceptions : Exception {
-		public NotFoundExceptions(string id) : base($"Entity with id {id} do not found") {}
-	}
-}
