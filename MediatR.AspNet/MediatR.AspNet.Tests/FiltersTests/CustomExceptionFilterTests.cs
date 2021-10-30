2d44c3db9a1ae5978feed147c372bada90e21b5d
LAPTOP-1BL52TMD\kasia <kasiakadziolka@gmail.com>
2021-10-30 16:54:40 +0200

    test init

---
+++ MediatR.AspNet/MediatR.AspNet.Tests/FiltersTests/CustomExceptionFilterTests.cs
@@ -0,0 +1,13 @@
+﻿using MediatR.AspNet.Exceptions;
+using NUnit.Framework;
+
+namespace MediatR.AspNet.Tests.FiltersTests {
+    public class CustomExceptionFilterTests {
+        [Test]
+        public void NotFoundException_ShouldReturnNotFound() {
+            // Arrange 
+            var exception = new NotFoundException();
+            
+        }
+    }
+}
