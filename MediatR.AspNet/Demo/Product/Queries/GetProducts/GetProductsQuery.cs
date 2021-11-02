0ba033f0b933efb988462c23c0fc4ff4239e3f2e
LAPTOP-1BL52TMD\kasia <kasiakadziolka@gmail.com>
2021-11-02 11:25:08 +0100

    Add Get query

---
+++ MediatR.AspNet/Demo/Product/Queries/GetProducts/GetProductsQuery.cs
@@ -0,0 +1,6 @@
+﻿using System.Collections.Generic;
+using MediatR.AspNet;
+
+namespace Demo.Product.Queries.GetProducts {
+    public class GetProductsQuery : IQuery<List<ProductDto>> { }
+}
---
+++ MediatR.AspNet/Demo/Product/Queries/GetProducts/GetProductsQueryHandler.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+
+namespace Demo.Product.Queries.GetProducts {
+    public class GetProductsQueryHandler : IRequestHandler<GetProductsQuery, List<ProductDto>> {
+        public GetProductsQueryHandler() {
+            
+        }
+
+        public Task<List<ProductDto>> Handle(GetProductsQuery request, CancellationToken cancellationToken) {
+            throw new System.NotImplementedException();
+        }
+    }
+}
