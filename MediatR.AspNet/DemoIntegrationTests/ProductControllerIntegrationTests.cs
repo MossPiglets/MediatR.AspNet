1a0144c8eb40cff4ad9a9e3bf6622ecec6d4c474
Katarzyna-Kadziolka <kasiakadziolka@gmail.com>
2021-11-04 18:32:33 +0100

    builder

--- MediatR.AspNet/DemoIntegrationTests/ProductControllerIntegrationTests.cs
+++ MediatR.AspNet/DemoIntegrationTests/ProductControllerIntegrationTests.cs
@@ -1,26 +1,40 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
 using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+using Demo;
+using Demo.Product;
+using FluentAssertions;
 using NUnit.Framework;
 
-namespace DemoIntegrationTests
-{
-    public class ProductControllerIntegrationTests
-    {
-        private HttpClient _client;
-        [OneTimeSetUp]
-        public void Setup() {
-            var factory = new DemoWebApplicationFactory();
-            _client = factory.CreateClient();
-        }
+namespace DemoIntegrationTests {
+	public class ProductControllerIntegrationTests {
+		private HttpClient _client;
 
-        [OneTimeTearDown]
-        public void CleanUp() {
-            _client.Dispose();
-        }
+		[OneTimeSetUp]
+		public void Setup() {
+			var factory = new DemoWebApplicationFactory();
+			_client = factory.CreateClient();
+		}
 
-        [Test]
-        public void Test1()
-        {
-            Assert.Pass();
-        }
-    }
+		[OneTimeTearDown]
+		public void CleanUp() {
+			_client.Dispose();
+		}
+
+		[Test]
+		public async Task GetProducts_ShouldReturnProductsList() {
+			// Arrange
+			var productsFromData = ProductsFactory.Products;
+			//Act
+			var response = await _client.GetAsync("api/Products");
+			response.StatusCode.Should().Be(HttpStatusCode.OK);
+			var products = await response.Content.ReadFromJsonAsync<List<ProductDto>>();
+			// Assert
+			products.Should().NotBeEmpty();
+			products.Should().ContainEquivalentOf(productsFromData.ToList());
+		}
+	}
 }
---
+++ MediatR.AspNet/DemoIntegrationTests/RequestBuilder.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using Bogus;
+using Demo.Product.Commands.PostProduct;
+using Demo.Product.Commands.PutProduct;
+
+namespace DemoIntegrationTests {
+	public static class RequestBuilder {
+		private static List<int> _existingIds;
+		private static Random _random;
+		static RequestBuilder() {
+			_existingIds = new List<int>();
+			for (int i = 1; i < 11; i++) {
+				_existingIds.Add(i);
+			}
+			_random = new Random();
+		}
+
+		public static int CreateExistingId() {
+			var id = 0;
+			while (id <= 0) {
+				id = _random.Next(11);
+			}
+			return id;
+		}
+
+		public static int CreateNotExistingId() {
+			var id = 1;
+			while (!_existingIds.Contains(id)) {
+				id = _random.Next(int.MaxValue);
+			}
+			_existingIds.Add(id);
+			
+			return id;
+		}
+
+		public static CreateProductCommand CreateProductCommand() {
+			var fakerCreateProductCommand = new Faker<CreateProductCommand>()
+			                   .RuleFor(a => a.Id, CreateNotExistingId)
+			                   .RuleFor(a => a.Name, f => f.Lorem.Word());
+			return fakerCreateProductCommand.Generate();
+		}
+
+		public static UpdateProductCommand UpdateProductCommand() {
+			var fakerUpdateProductCommand = new Faker<UpdateProductCommand>()
+			                   .RuleFor(a => a.OldProductId, CreateExistingId)
+			                   .RuleFor(a => a.Name, f => f.Lorem.Word())
+			                   .RuleFor(a => a.Id, CreateNotExistingId);
+			return fakerUpdateProductCommand.Generate();
+		}
+	}
+}
