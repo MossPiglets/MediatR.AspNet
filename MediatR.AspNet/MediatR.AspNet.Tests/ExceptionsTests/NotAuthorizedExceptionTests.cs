40a59298677c15b3969c0aaf8e2f7e969a95eecd
LAPTOP-1BL52TMD\kasia <kasiakadziolka@gmail.com>
2021-11-16 11:12:15 +0100

    Add unit tests

---
+++ MediatR.AspNet/MediatR.AspNet.Tests/ExceptionsTests/NotAuthorizedExceptionTests.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using FluentAssertions;
+using MediatR.AspNet.Exceptions;
+using NUnit.Framework;
+
+namespace MediatR.AspNet.Tests.ExceptionsTests {
+    public class NotAuthorizedExceptionTests {
+        [Test]
+        public void ShouldBeException() {
+            // Arrange
+            // Act
+            // Assert
+            typeof(NotAuthorizedException).Should().BeAssignableTo(typeof(Exception));
+        }
+
+        [Test]
+        public void NoArguments_ShouldReturnExceptionWithBaseMessage() {
+            // Arrange
+            // Act
+            var exception = new NotAuthorizedException();
+            // Assert
+            exception.Message.Should().Be("User is not allowed to access entity");
+        }
+
+        [Test]
+        public void EntityType_ShouldReturnExceptionWithEntityType() {
+            // Arrange
+            var type = typeof(string);
+            // Act
+            var exception = new NotAuthorizedException(type);
+            // Assert
+            exception.Message.Should().Be($"User is not allowed to access {type.Name}");
+        }
+        [Test]
+        public void EntityTypeAndId_ShouldReturnExceptionWithEntityTypeAndId() {
+            // Arrange
+            var type = typeof(string);
+            var id = "000";
+            // Act
+            var exception = new NotAuthorizedException(type, id);
+            // Assert
+            exception.Message.Should().Be($"User is not allowed to access {type.Name} with id {id}");
+        }
+        [Test]
+        public void MessageAndInnerException_ShouldReturnExceptionWithMessageAndInnerException() {
+            // Arrange
+            var innerException = new ArgumentException();
+            var message = "test";
+            // Act
+            var exception = new NotAuthorizedException(message, innerException);
+            // Assert
+            exception.Message.Should().Be(message);
+            exception.InnerException.Should().Be(innerException);
+        }
+        [Test]
+        public void Message_ShouldReturnExceptionWithMessage() {
+            // Arrange
+            var message = "test";
+            // Act
+            var exception = new NotAuthorizedException(message);
+            // Assert
+            exception.Message.Should().Be(message);
+        }
+    }
+}
--- MediatR.AspNet/MediatR.AspNet/Exceptions/NotAuthorizedException.cs
+++ MediatR.AspNet/MediatR.AspNet/Exceptions/NotAuthorizedException.cs
@@ -5,7 +5,7 @@ namespace MediatR.AspNet.Exceptions {
         public NotAuthorizedException() : base("User is not allowed to access entity") { }
         public NotAuthorizedException(Type entityType) : base($"User is not allowed to access {entityType.Name}") { }
         public NotAuthorizedException(Type entityType, string id) : base(
-            $"User is not allowed to access {entityType.Name} with {id}") { }
+            $"User is not allowed to access {entityType.Name} with id {id}") { }
         public NotAuthorizedException(string message, Exception innerException) : base(message, innerException) { }
         public NotAuthorizedException(string message) : base(message) { }
     }
