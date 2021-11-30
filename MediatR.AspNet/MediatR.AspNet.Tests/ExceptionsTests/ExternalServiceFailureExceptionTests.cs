c9fada840ef937b43ccb144afd17d75a182ca965
LAPTOP-1BL52TMD\kasia <kasiakadziolka@gmail.com>
2021-11-30 15:48:51 +0100

    ExternalServiceFailureException

---
+++ MediatR.AspNet/MediatR.AspNet.Tests/ExceptionsTests/ExternalServiceFailureExceptionTests.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using FluentAssertions;
+using MediatR.AspNet.Exceptions;
+using NUnit.Framework;
+
+namespace MediatR.AspNet.Tests.ExceptionsTests {
+    public class ExternalServiceFailureExceptionTests {
+        [Test]
+        public void ShouldBeException() {
+            // Arrange
+            // Act
+            // Assert
+            typeof(ExternalServiceFailureException).Should().BeAssignableTo(typeof(Exception));
+        }
+
+        [Test]
+        public void NoArguments_ShouldReturnExceptionWithBaseMessage() {
+            // Arrange
+            // Act
+            var exception = new ExternalServiceFailureException();
+            // Assert
+            exception.Message.Should().Be("External service failed");
+        }
+
+        [Test]
+        public void MessageAndInnerException_ShouldReturnExceptionWithMessageAndInnerException() {
+            // Arrange
+            var innerException = new ArgumentException();
+            var message = "test";
+            // Act
+            var exception = new ExternalServiceFailureException(message, innerException);
+            // Assert
+            exception.Message.Should().Be(message);
+            exception.InnerException.Should().Be(innerException);
+        }
+
+        [Test]
+        public void Message_ShouldReturnExceptionWithMessage() {
+            // Arrange
+            var message = "test";
+            // Act
+            var exception = new ExternalServiceFailureException(message);
+            // Assert
+            exception.Message.Should().Be(message);
+        }
+    }
+}
