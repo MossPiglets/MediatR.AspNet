d75151da6bc8df6020fb15ca763269a72fe5aa0c
GitHub Action <ACTION@github.com>
2024-02-23 10:02:22 +0100

    ToProblemdetailsTests

---
+++ MediatR.AspNet/MediatR.AspNet.Tests/ExceptionsTests/BaseApplicationExceptionTests.cs
@@ -0,0 +1,22 @@
+﻿using FluentAssertions;
+using Microsoft.AspNetCore.Http;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace MediatR.AspNet.Tests.ExceptionsTests;
+
+public class BaseApplicationExceptionTests {
+    [Test]
+    public void ToProblemDetails_ShouldReturnProblemDetails() {
+        // Arrange
+        var exception = Substitute.For<BaseApplicationException>("Test code", StatusCodes.Status403Forbidden, "Test message");
+        
+        // Act
+        var problemDetails = exception.ToProblemDetails();
+        
+        // Assert
+        problemDetails.Message.Should().Be(exception.Message);
+        problemDetails.Code.Should().Be(exception.Code);
+        problemDetails.Status.Should().Be(exception.Status);
+    }
+}
